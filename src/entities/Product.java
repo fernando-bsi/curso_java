7acb576b89919bbbf3841ef18f1e389450a0139d
Fernando Fernandes <fernando.fernandes@PPM-SPO-14684.local>
2021-05-23 15:20:33 -0300

    Adicionando o construtor product - aula 76

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -5,6 +5,14 @@ public class Product {
     public double price;
     public int quantity;
 
+    //this referencia para o próprio objeto
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
     public double totalValueInStock(){
         return price * quantity;
     }
